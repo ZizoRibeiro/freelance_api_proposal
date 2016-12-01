4d0ef0ee77dfac07f463121aa3678ae959159e27
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-12-01 13:12:44 -0800

    Created API Microservice and sample data for Proposals

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |proposal|
+ Proposal.create!( 
+    customer: "Customer #{proposal}",
+    portfolio_url: "https://github.com/ZizoRibeiro" ,
+    tools: "Ruby on Rails, Postgresql, Css, AngularJs",
+    estimate_hours: (90 + proposal),
+    hourly_rate: 95,
+    weeks_to_complete: 12,
+    client_email: "zizo@microhelpinfo.com.br",
+  )
+
+end
+
+
